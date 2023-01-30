402b85a83d85958a9869b6c9759e35778f493b7f
luqqmann28 <123887250+luqqmann28@users.noreply.github.com>
2023-01-30 09:45:22 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.navbar');
+document.addEventListener('scroll', () => {
+    if(window.top.scrollY > 19){
+        navbar.classList.add('scroll');
+    }else{
+        navbar.classList.remove('scroll');
+        navbar.style.transition = '.4s ease';
+    }
+})
